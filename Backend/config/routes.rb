ed808f58d90444606b817be837c5b2f79032f51f
Gladdy17 <gladston.bosco@gmail.com>
2025-04-04 19:21:18 -0600

    Added routes rentalproperties

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show]
+  resources :rental_properties 
      # your custom UsersController for profile viewing remains separate
     # other routes...
   end
