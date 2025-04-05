5e9ab86675ec3e1fa6fd3a6aacb091e393426937
Gladdy17 <gladston.bosco@gmail.com>
2025-04-04 19:09:09 -0600

    Added rental_property model

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     devise_for :users, controllers: { registrations: 'users/registrations' }
-    resources :users, only: [:show]  # your custom UsersController for profile viewing remains separate
+  resources :users, only: [:show]
+     # your custom UsersController for profile viewing remains separate
     # other routes...
   end
   
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_29_190728) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_05_010305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "rental_properties", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "address"
+    t.integer "sq_feet"
+    t.integer "bedrooms"
+    t.integer "bathrooms"
+    t.string "property_types"
+    t.decimal "fees"
+    t.boolean "utilities_included"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_rental_properties_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +45,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_29_190728) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rental_properties", "users"
 end
