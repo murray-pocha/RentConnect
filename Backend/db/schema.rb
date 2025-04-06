4b7b019e8e436cd63fdaaa50a63a241738c6978a
Gladdy17 <gladston.bosco@gmail.com>
2025-04-06 10:27:28 -0600

    added rental_applications.rb

--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_05_010305) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-ActiveRecord::Schema[7.1].define(version: 2025_04_05_155008) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_06_160847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +40,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_05_155008) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "rental_applications", force: :cascade do |t|
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "rental_property_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["rental_property_id"], name: "index_rental_applications_on_rental_property_id"
+    t.index ["user_id"], name: "index_rental_applications_on_user_id"
   end
 
   create_table "rental_properties", force: :cascade do |t|
@@ -79,5 +85,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_05_155008) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "rental_applications", "rental_properties"
+  add_foreign_key "rental_applications", "users"
   add_foreign_key "rental_properties", "users"
 end
