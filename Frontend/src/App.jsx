b6c1caa4ebefbc866c0ed696d97d6c29f722357f
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-05 21:40:42 -0700

    comments for development instructions

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,8 +6,11 @@ import LandLordDashboard from './components/LandlordDashboard'
 
 function App() {
 
-  const loggedIn = true //Rplace with state logic to authenticate user
-  const isTenant = true //Rplace with state logic to authenticate user
+
+  //Placeholder variables to navigate the site until authentication and routes are implemented
+
+  const loggedIn = true //Replace with state logic to authenticate user
+  const isTenant = true //Replace with state logic to authenticate user
 
   return (
     loggedIn ? 
