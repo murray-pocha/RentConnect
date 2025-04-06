db711fdd5c11a6901ddc37e185c60685bb98fa20
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-05 22:24:46 -0700

    created mypropertiesbtn component

--- Frontend/src/components/Sidenav.jsx
+++ Frontend/src/components/Sidenav.jsx
@@ -3,13 +3,30 @@ import "../styles/Sidenav.css"
 import MyprofBtn from "./MyProfBtn";
 import ViewPropertyBtn from "./ViewPropertyBtn";
 import AccountSettingsBtn from "./AccountSettingsBtn";
+import MyPropertiesBtn from "./MyPropertiesBtn";
 
 function Sidenav() {
+
+  let isTenant = true
+
   return (
     <div className="sidenav_container">
-      <MyprofBtn />
-      <ViewPropertyBtn />
-      <AccountSettingsBtn />
+      {isTenant ? (
+        <>
+          <MyprofBtn />
+          <ViewPropertyBtn />
+          <AccountSettingsBtn />
+        </>
+      ) :
+
+      (
+        <>
+          <MyprofBtn />
+          <MyPropertiesBtn />
+          <AccountSettingsBtn />
+        </>
+      )
+      }
     </div>
   )
 }
