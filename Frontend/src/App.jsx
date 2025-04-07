bdc24cb5569c3b94a7db22a0fcaa8e171f5c7511
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-06 18:50:26 -0700

    landlord dashboard removed, will use tenant dashboard for both users

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './App.css'
 import LoginPage from './components/LoginPage'
 import TenantDashboard from './components/TenantDashboard'
-import LandLordDashboard from './components/LandlordDashboard'
 
 function App() {
 
@@ -10,18 +9,13 @@ function App() {
   //Placeholder variables to navigate the site until authentication and routes are implemented
 
   const loggedIn = true //Replace with state logic to authenticate user
-  const isTenant = true //Replace with state logic to authenticate user
 
   return (
     loggedIn ? 
-      isTenant ? (
         <TenantDashboard />
-      ) : (
-        <LandLordDashboard />
-      )
-      : (
+      :
         <LoginPage />
-      ))
+      )
 }
 
 export default App
