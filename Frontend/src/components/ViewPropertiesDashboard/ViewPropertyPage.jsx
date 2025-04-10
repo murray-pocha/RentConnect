4c28bad5d15d1cfb1bf398d54936e84ce349222e
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-09 23:53:39 -0700

    mapbox rendering in app

---
+++ Frontend/src/components/ViewPropertiesDashboard/ViewPropertyPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MapContainer from "./MapContainer";
+import '../../styles/viewPropertyPage.css'
+
+function ViewPropertyPage () {
+
+  return (
+    <div id="map_container">
+      <h1>View Properties</h1>
+      <MapContainer />
+    </div>
+  )
+
+}
+
+export default ViewPropertyPage
