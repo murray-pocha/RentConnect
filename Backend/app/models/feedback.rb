ec59b43605d82ce56570774bdfba9e3a1c0a1db3
Gladdy17 <gladston.bosco@gmail.com>
2025-04-15 21:03:08 -0600

    Added Ratings for feedback

--- Backend/app/models/feedback.rb
+++ Backend/app/models/feedback.rb
@@ -1,4 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :recipient, class_name: 'User'
+
+  validates :rating, presence: true, inclusion: { in: 1..5}
 end
