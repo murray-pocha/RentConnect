88d49eeb843d6d54eaf93012a3f9e361735b86f1
jaidenpearson <jaidensrpearson@gmail.com>
2025-04-24 19:53:55 -0700

    logout clears localstorage and redirects to login page

--- Frontend/src/components/Button Components/LogoutButton.jsx
+++ Frontend/src/components/Button Components/LogoutButton.jsx
@@ -18,7 +18,7 @@ function LogoutButton({ setLoggedIn }) {
       console.log("User logged out successfully");
 
       localStorage.clear()
-      navigate("/")
+      navigate("/sign-up")
     } catch (error) {
       console.error("Error logging out:", error);
     }
